ac1c511f49a7652096afce539bf336f9e5e3cd43
Ethan Pineda <joshuapineda66@gmail.com>
2024-02-23 11:16:28 -0600

    step 4 done

--- src/_components/Calendar.jsx
+++ src/_components/Calendar.jsx
@@ -1,10 +1,61 @@
 import React from "react"; 
+import Event from "./Event";
 
 export default function Calendar() {
 
     return (
         <div className="Calendar">
-            Testing the calendar component
+            <table>
+                <thead>
+                    <tr>
+                        <th>Sunday</th>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="time">8 am</td>
+                    </tr>
+                    <tr>
+                        <td className="time">9 am</td>
+                    </tr>    
+                    <tr>
+                        <td className="time">9 am</td>
+                    </tr>  
+                    <tr>
+                        <td className="time">9 am</td>
+                    </tr>
+                    <tr>
+                        <td className="time">10 am</td>
+                    </tr>
+                    <tr>
+                        <td className="time">11 am</td>
+                    </tr>
+                    <tr>
+                        <td className="time">12 pm</td>
+                    </tr>
+                    <tr>
+                        <td className="time">1 pm</td>
+                    </tr>
+                    <tr>
+                        <td className="time">2 pm</td>
+                    </tr>
+                    <tr>
+                        <td className="time">3 pm</td>
+                    </tr>
+                    <tr>
+                        <td className="time">4 pm</td>
+                    </tr>
+                    <tr>
+                        <td className="time">5 pm</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 };
---
+++ src/_components/Event.jsx
@@ -0,0 +1,11 @@
+import React from "react"; 
+
+export default function Event(props) {
+
+    return (
+        <td className="Event">
+            <h5>Test Event Name</h5>
+        </td>
+    );
+
+};
