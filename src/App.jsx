3ca96c40b9075eb14c22f0ce40cece858d15f334
Ethan Pineda <joshuapineda66@gmail.com>
2024-02-23 11:02:39 -0600

    step 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css"; 
-
+import Calendar from "./_components/Calendar";
 const App = () => {
 
   return (
     <div className="App">
       <h1>Time Tabled</h1>
       <h2>This is your current schedule</h2>
+      <Calendar /> 
     </div>
   );
 };
